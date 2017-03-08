2339f6fe64590477e6e8b85235ba838a33df00ea
leongfeng <leongfeng@163.com>
2017-03-08 22:03:47 +0800

    Initial commit

---
+++ src/com/tts/AlgorithmsTest.java
@@ -0,0 +1,21 @@
+package com.tts;
+
+import org.junit.Test;
+
+/**
+ * @author: mike
+ * @since: 2017/3/6
+ */
+public class AlgorithmsTest {
+
+    @Test
+    public void test1(){
+        System.out.println(gcd(8,4));
+    }
+
+    public static int gcd(int p, int q){
+        if(q==0) return p;
+        int r = p % q;
+        return gcd(q, r);
+    }
+}
